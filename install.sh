950c05c2067ed603f511441a213876a6db301b4e
Joo Sung <joohsung@gmail.com>
2020-04-17 06:18:05 -0400

    AAI CentOS8 - APM AUTO INSTALL V.1.0

--- install.sh
+++ install.sh
@@ -51,6 +51,38 @@ echo ""
 echo ""
 echo ""
 
+echo "
+ =======================================================
+
+               < phpMyAdmin 설치 하기>
+
+ =======================================================
+"
+echo "phpMyAdmin 설치 하시겠습니까? 'Y' or 'N'"
+read YN
+YN=`echo $YN | tr "a-z" "A-Z"`
+ 
+if [ "$YN" != "Y" ]
+then
+    echo "설치 중단."
+    exit
+fi
+
+echo""
+echo "phpMyAdmin 설치를 시작 합니다."
+cd /root/CentOS8_AAI/APM
+
+chmod 700 phpMyAdmin.sh
+
+sh phpMyAdmin.sh
+
+echo ""
+echo ""
+echo "phpMyAdmin 설치 완료!"
+echo ""
+echo ""
+echo ""
+
 #설치 파일 삭제
 rm -rf /root/CentOS8_AAI/APM
 echo ""
